189581d45c8dcc65372458913efde15810cde6f3
Matt Nicholson <matt.nicholson@appcarousel.com>
2016-09-26 12:54:26 -0600

    Added better error handling for mysql

--- src/app.js
+++ src/app.js
@@ -6,16 +6,24 @@ const knex = require('./middleware/database');
 const app = module.exports = new koa();
 
 app.use(knex);
-app.use(function *(){
-  const rows = yield this.knex.raw('show status');
+app.use(function* () {
   const dbStatus = {};
-  for(const v of rows[0]){
-    dbStatus[v.Variable_name] = v.Value
-  }  
+  try {
+    const rows = yield this.knex.raw('show status');
+    for (const v of rows[0]) {
+      dbStatus[v.Variable_name] = v.Value
+    }
+  } catch (e) {
+    this.status = 500;
+    return this.body = {
+      message: e.message,
+      error: e
+    }
+  }
 
   this.body = {
     database: dbStatus
-  }
+  };
 });
 
 if (!module.parent) {
